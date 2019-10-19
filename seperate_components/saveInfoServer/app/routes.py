7aa67099ebf9854d1f5690d054e92a76cc8d0375
Minh An Dao <minhan7497@gmail.com>
2019-10-19 12:37:58 +0700

    configure to close the server when done and icon img

--- seperate_components/saveInfoServer/app/routes.py
+++ seperate_components/saveInfoServer/app/routes.py
@@ -8,7 +8,7 @@
  *  html routes for functions of the server
  *
  --------------------------------------------------------------"""
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, flash, redirect, url_for, request
 from app import app, db
 from app.forms import InfoForm
 from app.models import User
@@ -24,9 +24,7 @@ def index():
             newUser = User(name=form.name.data, mssv=form.mssv.data)
             db.session.add(newUser)
             db.session.commit()
-            flash('Information received:\n Name: {}\n - ID: {}'.format(
-                   form.name.data, form.mssv.data))
-        return redirect(url_for('index'))
+        return redirect(url_for('gotInfo'))
     templateData = {
         'server_title': 'MIS Locker System',
         'server_func': 'Add Guest ID',
@@ -34,6 +32,21 @@ def index():
     }
     return render_template('index.html', **templateData)
 
+@app.route('/gotinfo', methods=['GET', 'POST'])
+def gotInfo():
+    user = User.query.order_by(User.id.desc()).first()
+    templateData = {
+        'server_title': 'MIS Locker System',
+        'server_func': 'Information received',
+        'user_name': user.name,
+        'user_id': user.mssv
+    }
+    # Start shutting down server
+    func = request.environ.get('werkzeug.server.shutdown')
+    if func is None:
+        raise RuntimeError('Not running with the Werkzeug Server')
+    func()
+    return render_template('gotInfo.html', **templateData)
 
 @app.route('/about')
 def about():
