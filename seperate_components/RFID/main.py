fc8a7c923efa2fc8dd7048df55cb0c4fda5fec0c
Minh An Dao <minhan7497@gmail.com>
2019-10-24 16:42:17 +0700

    add rfid

--- seperate_components/RFID/main.py
+++ seperate_components/RFID/main.py
@@ -9,15 +9,50 @@
  *
  *
  --------------------------------------------------------------"""
-from rfid import Gwiot_7304D2 as RFID
+# !/usr/bin/env python
 
+# green/data0 is pin 22
+# white/data1 is pin 7
+import time
+import RPi.GPIO as GPIO
+
+D0 = 21
+D1 = 20
+bits = ''
+timeout = 5
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(D0, GPIO.IN)
+GPIO.setup(D1, GPIO.IN)
+
+
+def one(channel):
+    global bits
+    bits |= '1'
+
+
+def zero(channel):
+    global bits
+    bits |= '0'
+
+
+GPIO.add_event_detect(D0, GPIO.FALLING, callback=zero)
+GPIO.add_event_detect(D1, GPIO.FALLING, callback=one)
+
+print("Present Card")
 try:
-    rfid = RFID()
-    print('RFID System ready!')
     while True:
-        if rfid.available():
-            print(rfid.read())
-            print('done!')
-
+        if len(bits) == 32:
+            # time.sleep(0.1)
+            # print "Binary:",bits
+            print("Decimal:", int(str(bits), 2))
+            print("Hex:",hex(int(str(bits),2)))
+            bits = '0'
+            # time.sleep(0.1)
 except KeyboardInterrupt:
-    pass
+    GPIO.cleanup()
+    print
+    "Clean Exit By user"
+
+GPIO.cleanup()
