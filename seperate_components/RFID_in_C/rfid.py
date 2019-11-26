6348a6f67daf6e86210608bd36446473654d96c4
Minh An Dao <minhan7497@gmail.com>
2019-11-26 14:43:45 +0700

    add RDM6300

--- seperate_components/RFID_in_C/rfid.py
+++ seperate_components/RFID_in_C/rfid.py
@@ -40,32 +40,90 @@
  *
  --------------------------------------------------------------"""
 import subprocess as subpro
+from pyrfid.pyrfid import PyRfid
 from ContinuousStreamRead import StreamReader
 import sys
 
-TARGET = './rfid_main'
-rfid_object = object()
-rfid_mes = object()
 
-def start():
-    global rfid_object, rfid_mes
-    rfid_object = subpro.Popen([TARGET], shell=False, stdout=subpro.PIPE, stderr=subpro.PIPE)
-    rfid_mes = StreamReader(rfid_object.stdout)
-    print('RFID ready!')
+class RDM6300:
+    def __init__(self, port='/dev/ttyUSB0', baudRate=9600):
+        self.rfid = PyRfid(port, baudRate)
+        print('RFID ready!')
 
-def check():
-    mes = rfid_mes.readline(0.05)  # 0.05 secs to let the shell output the result
-    sys.stdout.flush()
-    if mes is not None:  # turn it into string if it is not a null
-        mes = mes.strip().decode("utf-8")
-    if mes is None or mes == 'CHECKSUM_FAILED':
-        return [False, '']
+    def hasID(self):
 
-    return [True, mes]
+    def tagID(self):
+        return self.rfid.tagId
 
-def stop():
-    # check if process terminated or not
-    if rfid_object.poll() is None:
-        rfid_object.terminate()
-        rfid_object.kill()
-        print('RFID terminated!')
+    def debugInfo(self):
+        print('RAW:      ' + self.rfid.rawTag)
+        print('Checksum: ' + self.rfid.tagChecksum + '\n')
+
+        print('Decimal-format:')
+        print('ID:       ' + self.rfid.tagId)
+        print('Type:     ' + self.rfid.tagType + '\n')
+
+        print('Float-format:')
+        print('ID:       ' + self.rfid.tagIdFloat)
+        print('Type:     ' + self.rfid.tagTypeFloat)
+
+    def stop(self):
+        return
+
+class Gwiot_7304D2:
+    def __init__(self):
+        self.TARGET = './rfid_main'
+        self.rfid = subpro.Popen([self.TARGET], shell=False, stdout=subpro.PIPE, stderr=subpro.PIPE)
+        self.mes = StreamReader(self.rfid.stdout)
+        self.tag_id = ''
+        print('RFID ready!')
+
+    def hasID(self):
+        mes = self.mes.readline(0.05)  # 0.05 secs to let the shell output the result
+        sys.stdout.flush()
+        if mes is not None:  # turn it into string if it is not a null
+            self.tag_id = mes.strip().decode("utf-8")
+            return True
+        if mes is None or mes == 'CHECKSUM_FAILED':
+            return False
+
+    def tagID(self):
+        return self.tag_id
+
+    def debugInfo(self):
+        print('ID:       '+ self.tag_id)
+        print('Nothing else to display here')
+
+    def stop(self):
+        # check if process terminated or not
+        if self.rfid.poll() is None:
+            self.rfid.terminate()
+            self.rfid.kill()
+            print('RFID terminated!')
+
+
+
+# def start():
+#     global rfid_object, rfid_mes
+#     rfid_object = subpro.Popen([TARGET], shell=False, stdout=subpro.PIPE, stderr=subpro.PIPE)
+#     rfid_mes = StreamReader(rfid_object.stdout)
+#     print('RFID ready!')
+#
+#
+# def check():
+#     mes = rfid_mes.readline(0.05)  # 0.05 secs to let the shell output the result
+#     sys.stdout.flush()
+#     if mes is not None:  # turn it into string if it is not a null
+#         mes = mes.strip().decode("utf-8")
+#     if mes is None or mes == 'CHECKSUM_FAILED':
+#         return [False, '']
+#
+#     return [True, mes]
+#
+#
+# def stop():
+#     # check if process terminated or not
+#     if rfid_object.poll() is None:
+#         rfid_object.terminate()
+#         rfid_object.kill()
+#         print('RFID terminated!')
