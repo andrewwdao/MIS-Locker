28a0b21c378f8fe9eb1147924b79add507526806
Minh-An Dao <minhan7497@gmail.com>
2020-03-18 16:38:27 +0700

    try to find error

--- main/main.py
+++ main/main.py
@@ -1040,7 +1040,7 @@ if __name__ == '__main__':
         subpro.Popen(['python3','syshalt.py'], shell=False) # closing procedure
         rfid.stop()  # REMEMBER TO DO THIS SINCE THE READING IN C DON'T EXIT BY ITSELF!
         
-    except (OSError, Exception): # I/O error or exception
-        lcd.systemErrorPage()
-        pr.init()    # clear all locks and LEDs before shutdown
-        rfid.stop()  # REMEMBER TO DO THIS SINCE THE READING IN C DON'T EXIT BY ITSELF!
+    # except (OSError, Exception): # I/O error or exception
+    #     lcd.systemErrorPage()
+    #     pr.init()    # clear all locks and LEDs before shutdown
+    #     rfid.stop()  # REMEMBER TO DO THIS SINCE THE READING IN C DON'T EXIT BY ITSELF!
