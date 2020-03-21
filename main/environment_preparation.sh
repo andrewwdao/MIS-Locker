40261bb27a27a7be65b8325732b3234495731bbc
Minh-An Dao <minhan7497@gmail.com>
2020-03-21 17:52:37 +0700

    update dir

--- main/environment_preparation.sh
+++ main/environment_preparation.sh
@@ -190,7 +190,8 @@ fi
 /home/$(who am i | awk '{print $1}')/system/main/bluetooth_deactivate.sh
 
 # make system read-only
-/home/$(who am i | awk '{print $1}')/system/main/readonly/setup.sh yes
+cd /home/$(who am i | awk '{print $1}')/system/main/readonly
+./setup.sh yes
 
 echo 
 echo 
