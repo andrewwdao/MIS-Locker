a9acd011606131119ef78e06eb791df11bdc80a4
Minh-An Dao <minhan7497@gmail.com>
2020-03-27 17:38:53 +0700

    decrease debounce time

--- main/button.py
+++ main/button.py
@@ -11,7 +11,7 @@ import RPi.GPIO as GPIO  # default as BCM mode!
 import time
 
 # ---------------------------- Private Parameters:
-DEBOUNCE = 300  # xxx second
+DEBOUNCE = 100  # xxx second
 # -----Pinout
 UP_BUTTON = 5
 DOWN_BUTTON = 13 
