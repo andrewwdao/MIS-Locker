94093b412b7e5910c218207d3b6c6514c36e6052
Minh-An Dao <minhan7497@gmail.com>
2020-03-27 15:41:54 +0700

    testing 2

--- seperate_components/ADC_I2C/main.py
+++ seperate_components/ADC_I2C/main.py
@@ -7,7 +7,17 @@ button.init()
 while True:
     # print(switches.readRawM1())
     #print(switches.read())
-    if(button.read()=="BUT_OK"):
-        print("Hello world")
+    button_state = button.read()
+    if(button_state=="BUT_OK"):
+        print("Hello world OK")
+
+    if(button_state=="BUT_CANCEL"):
+        print("Hello world CANCEL")
+
+    if(button_state=="BUT_UP"):
+        print("Hello world UP")
+    
+    if(button_state=="BUT_DOWN"):
+        print("Hello world DOWN")
     #print(button.read())
     # time.sleep(1)
