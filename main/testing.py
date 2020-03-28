9f452fa09887afe1f14ea2a563e4952a943c3445
Minh-An Dao <minhan7497@gmail.com>
2020-03-28 16:55:58 +0700

    try new method to stop new server

--- main/testing.py
+++ main/testing.py
@@ -1,5 +1,9 @@
 from gevent.pywsgi import WSGIServer
+from gevent.event import Event
 from app import saveInfo_app
 
+stopper = Event()
+
 http_server = WSGIServer(('0.0.0.0', 7497), saveInfo_app)
-http_server.serve_forever()
+http_server.start()
+stopper.wait()
