a131b3dae4f90d0c94d75b17269f82be08d1f4ff
Minh-An Dao <minhan7497@gmail.com>
2020-03-29 17:10:17 +0700

    change geven signal declaration order

--- main/server.py
+++ main/server.py
@@ -1,4 +1,3 @@
-import gevent
 from gevent.pywsgi import WSGIServer
 from app import saveInfo_app
 import threading
@@ -10,10 +9,10 @@ class WebServer(threading.Thread):
     def __init__(self):
         super().__init__()
         self.pid = os.getpid()
-        gevent.signal(signal.SIGTERM, self.shutdown)
-
+        
     def run(self):
         self.server = gevent.pywsgi.WSGIServer(('0.0.0.0', 7497), saveInfo_app)
+        signal.signal(signal.SIGTERM, self.shutdown)
         self.server.serve_forever()
 
     # ======================== for development only =====================
